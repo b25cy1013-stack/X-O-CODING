3ba4d1c293288307345de30db441a7ef4a4a6922
b25cy1013-stack <b25cy1013@iitj.ac.in>
2026-04-11 11:18:49 +0530

    Update XO_CODING.c

--- XO_CODING.c
+++ XO_CODING.c
@@ -1,1 +1,23 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
+char board[3][3] ;
+ int scoreP1=0;
+int scoreP2=0;
+int draw=0;
+
+void resetBoar(
+{
+char num = '1';
+for( int i=0 ; i<3 ; i++){
+for(int j=0 ; j<3 ; j++) {
+board[i][j] = num++;
+}
+}
+}
+
+void displayBoard() {
+  printf("\n");
+printf("
